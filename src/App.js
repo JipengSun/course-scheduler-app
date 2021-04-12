d1f7d8457f28a4d6113b92e15fbf54c01cc53a94
Lukas <lukasbill@163.com>
2021-04-12 18:32:10 +0800

    Add course list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const schedule = {
+  'title':'CS Courses for 2020-2021',
+  'courses':[
+    {
+      'id':'F101',
+      'title':'CS Algorithm',
+      'meets':'MWF 11:00-11:50'
+    },
+    {
+      'id':'F110',
+      'title':'Database',
+      'meets':'MWF 12:00-13:50' 
+
+    }
+  ]
+  
+};
+
+const Banner = ({ title }) => (
+  <h1>{ title }</h1>
+);
+const CourseList = ({courses}) =>(
+  <div>
+    { courses.map(course => <Course course = {course}/>)}
+  </div>
+);
+const terms = {F:'Fall',W:'Winter',S:'Spring'};
+
+const getCourseTerm = course =>(
+  terms[course.id.charAt(0)]
+);
+
+const getCourseNumber = course=>(
+  course.id.slice(1,4)
+);
+
+const Course = ({course}) =>(
+  <button>
+    {getCourseTerm(course)} CS {getCourseNumber(course)}:{course.title}
+   </button>
+)
+const App = () => (
+  <div>
+    <Banner title = {schedule.title}/>
+    <CourseList courses={schedule.courses}/>
+  </div>
+);
 
 export default App;
