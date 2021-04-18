96fecccd08defbc1daba1ae2cf231f63aaa7ac1b
Lukas <lukasbill@163.com>
2021-04-18 14:00:06 +0800

    Fetch data from the remote website

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import 'rbx/index.css';
 import {Button, Container, Title} from 'rbx';
+/*
 const schedule = {
   'title':'CS Courses for 2020-2021',
   'courses':[
@@ -24,9 +25,10 @@ const schedule = {
   ]
   
 };
+*/
 
 const Banner = ({ title }) => (
-  <Title>{ title }</Title>
+  <Title>{ title || '[loading...]'}</Title>
 );
 const CourseList = ({courses}) =>(
   <Button.Group>
@@ -48,11 +50,28 @@ const Course = ({course}) =>(
     {getCourseTerm(course)} CS {getCourseNumber(course)}:{course.title}
    </Button>
 )
-const App = () => (
-  <Container>
-    <Banner title = {schedule.title}/>
-    <CourseList courses={schedule.courses}/>
-  </Container>
-);
+
+const App = () => {
+  const url = 'https://courses.cs.northwestern.edu/394/data/cs-courses.php';
+  const [schedule, setSchedule] = useState({ title:'', courses:[]});
+  useEffect( ()=>{
+    const fetchSchedule = async ()=>{
+      const response = await fetch(url);
+      if (!response.ok) throw response;
+      const json = await response.json();
+      setSchedule(json);
+    }
+    fetchSchedule();
+  }
+    ,[])
+  return(
+    <Container>
+      <Banner title = {schedule.title}/>
+      <CourseList courses={schedule.courses}/>
+    </Container>
+  )
+  
+};
+
 
 export default App;
