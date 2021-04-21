554a5f9c24a6532c986f94a2d4602cbbd468fe4d
Lukas <lukasbill@163.com>
2021-04-21 17:34:26 +0800

    Add conflict detection

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ const Banner = ({ title }) => (
 );
 
 const terms = {F:'Fall',W:'Winter',S:'Spring'};
-
+const days = ['M','Tu','W','Th','F'];
 
 const getCourseTerm = course =>(
   terms[course.id.charAt(0)]
@@ -50,29 +50,124 @@ const TermSelector = ({state})=>(
   <Button.Group hasAddons>
     {
       Object.values(terms).map( value => 
-      <Button key = {value}
-      color = {buttonColor(value === state.term)}
-      onClick = { () => state.setTerm(value)}
+      <Button 
+        key = {value}
+        color = {buttonColor(value === state.term)}
+        onClick = { () => state.setTerm(value)}
       > {value} </Button>
       )
     }
   </Button.Group>
 );
 
-const Course = ({course}) =>(
-  <Button>
+
+/*
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions/Cheatsheet
+Regular expression rules:
+'//' means the start of the reg expression
+^ matches the begining of the input
+$ matches the end of input
+x|y matches either x or y
+(?:x) matches x but doesn't remember the match
+x+ matches the preceding item x 1 or more times
+\d matches any digit
+x? matches x 0 or 1 times
+ */
+
+// Original: "MWF 10:00-10:50"
+// Parsed: { days: "TuTh" hours: { start: 600, end: 680 } }
+const meetsPat = /^((?:M|Tu|W|Th|F)+) +(\d\d?):(\d\d)[ -](\d\d?):(\d\d)$/;
+
+const timeParts = meets => {
+  const[match,days,hh1,mm1,hh2,mm2] = meetsPat.exec(meets) || [];
+  return !match ?{} : {
+    days,
+    hours:{
+      start: hh1 * 60 + mm1 * 1,
+      end: hh2 * 60 + mm2 * 1
+    }
+  };
+};
+
+// Spread Operators '...': https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_syntax
+const addCourseTimes = course => (
+  {
+    ...course,
+    ...timeParts(course.meets)
+  }
+);
+const addScheduleTimes = schedule =>(
+  {
+    title: schedule.title,
+    courses: schedule.courses.map(addCourseTimes)
+  }
+)
+
+// Return True if course1 conflicts with course2
+const courseConflict = (course1, course2) =>(
+  course1 !== course2 &&
+  getCourseTerm(course1) === getCourseTerm(course2) &&
+  timeConflict(course1,course2)
+);
+
+// Return True if course1 conflicts with course2
+const timeConflict = (course1,course2)=>(
+  daysOverlap(course1.days,course2.days) && hoursOverlap(course1.hours, course2.hours)
+);
+
+const daysOverlap = (days1, days2) =>{
+  return days.some(day => days1.includes(day) && days2.includes(day))
+};
+
+const hoursOverlap = (hours1, hours2)=>{
+  if (Math.max(hours1.start, hours2.start) < Math.min(hours1.end, hours2.end)){
+    console.log(hours1,hours2)
+  }
+  return Math.max(hours1.start, hours2.start) < Math.min(hours1.end, hours2.end)
+};
+
+// Return True if the course is conflict with the selected courses.
+const hasConflict = (course, selected)=>{
+  //console.log(selected)
+  return selected.some(selection => courseConflict(course,selection))
+};
+
+
+
+
+const Course = ({course, state}) =>(
+  <Button 
+    color = { buttonColor(state.selected.includes(course))}
+    onClick = {()=> state.toggle(course)}
+    disabled = { hasConflict(course, state.selected)}
+  >
     {getCourseTerm(course)} CS {getCourseNumber(course)}:{course.title}
    </Button>
 )
 
+// selected is an array store the selected courses
+const useSelection = ()=>{
+  const [selected, setSelected] = useState([]);
+  const toggle = (x)=>{
+    setSelected(selected.includes(x) ? selected.filter(y => y!== x) : [x].concat(selected))
+  };
+  return [ selected, toggle];
+
+}
+
 const CourseList = ({courses}) =>{
   const [term,setTerm] = useState('Fall');
   const termCourses = courses.filter(course => term === getCourseTerm(course));
+  const [selected, toggle] = useSelection();
   return(
     <React.Fragment>
       <TermSelector state = {{term, setTerm}}/>
       <Button.Group>
-        { termCourses.map(course => <Course key={course.id} course = {course}/>)}
+        { termCourses.map(course => 
+        <Course 
+          key={course.id} 
+          course = {course}
+          state = { {selected, toggle}}/>)}
       </Button.Group>
     </React.Fragment>
   )
@@ -86,7 +181,7 @@ const App = () => {
       const response = await fetch(url);
       if (!response.ok) throw response;
       const json = await response.json();
-      setSchedule(json);
+      setSchedule(addScheduleTimes(json));
     }
     fetchSchedule();
   }
